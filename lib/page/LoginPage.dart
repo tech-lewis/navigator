570fd1c4fe43d87bd2c7ce6b0f8e774dfce553e9
Mark <techliujing@gmail.com>
2023-09-15 16:10:53 +0800

    修改登录页面

--- lib/page/LoginPage.dart
+++ lib/page/LoginPage.dart
@@ -3,10 +3,32 @@ import 'package:navigator/common/style/MyStyle.dart';
 import 'package:navigator/widget/GSYFlexButton.dart';
 import 'package:navigator/widget/GSYInputWidget.dart';
 import 'package:navigator/common/utils/NavigatorUtils.dart';
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
 
   static final String sName  = "login";
+  @override
+  State createState () {
+    return new _LoginPageState();
+  }
+}
+class _LoginPageState extends State<LoginPage>{
+  var _userName = "";
+  var _password = "";
 
+  final TextEditingController userController = new TextEditingController();
+  final TextEditingController pwController = new TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // initParameters();
+    userController.value = new TextEditingValue(text: "admin");
+    pwController.value = new TextEditingValue(text: "1111");
+  }
+
+  initParameters () async {
+    
+  }
   @override
   Widget build(BuildContext context) {
     return new Container(
@@ -25,15 +47,45 @@ class LoginPage extends StatelessWidget {
                       children: <Widget>[
                         new Image(image: new AssetImage('static/images/logo.png'), width: 80.0, height: 80.0),
                         new Padding(padding: new EdgeInsets.all(10.0)),
-                        new GSYInputWidget(hintText: "11111", iconData: Icons.account_circle),
+                        new GSYInputWidget(hintText: GSYStrings.login_username_hint_text, iconData: Icons.account_circle, onChanged: (String val) { _userName = val;}, controller: userController,),
                         new Padding(padding: new EdgeInsets.all(10.0)),
-                        new GSYInputWidget(hintText: "11111", iconData: Icons.lock_open, obscureText: true),
+                        new GSYInputWidget(hintText: GSYStrings.login_password_hint_text, iconData: Icons.lock_open, obscureText: true, onChanged: (String val) { _password = val;}, controller: pwController,),
                         new Padding(padding: new EdgeInsets.all(8.0)),
                         new GSYFlexButton(
                           text: GSYStrings.login_text,
                           color: Color(GSYColors.primaryValue),
                           textColor: Color(GSYColors.textWhite),
                           onPress: () {
+                            // if (_userName == null || _userName.length == 0) {
+                            //   showDialog(
+                            //     barrierDismissible: false,
+                            //     context: context,
+                            //     builder: (BuildContext context) {
+                            //       return AlertDialog(
+                            //         content: Text("帐号或密码不能为空 >_<"),
+                            //         actions: <Widget>[
+                            //           new FlatButton(child: new Text('确定', style: TextStyle(fontSize: 16.0)), onPressed: () {},)
+                            //         ],
+                            //       );
+                            //     }
+                            //   );
+                            //   return; 
+                            // }
+                            // if (_password == null || _password.length == 0) {
+                            //   showDialog(
+                            //     barrierDismissible: false,
+                            //     context: context,
+                            //     builder: (BuildContext context) {
+                            //       return AlertDialog(
+                            //         content: Text("帐号或密码不能为空 >_<"),
+                            //         actions: <Widget>[
+                            //           new FlatButton(child: new Text('确定', style: TextStyle(fontSize: 16.0)), onPressed: () {},)
+                            //         ],
+                            //       );
+                            //     }
+                            //   );
+                            //   return; 
+                            // }
                             NavigatorUtils.goHome(context);
                           },
                         )
--- lib/widget/GSYInputWidget.dart
+++ lib/widget/GSYInputWidget.dart
@@ -2,40 +2,50 @@ import 'package:flutter/material.dart';
 
 /// 带图标的输入框
 class GSYInputWidget extends StatefulWidget {
-  // 是否明文显示
   final bool obscureText;
+
   final String hintText;
 
   final IconData iconData;
 
-  GSYInputWidget({Key key, this.hintText, this.iconData, this.obscureText}) : super(key: key);
+  final ValueChanged<String> onChanged;
+
+  final TextStyle textStyle;
+
+  final TextEditingController controller;
+
+  GSYInputWidget({Key key, this.hintText, this.iconData, this.onChanged, this.textStyle, this.controller, this.obscureText = false}) : super(key: key);
 
   @override
-  _GSYInputWidgetState createState() => new _GSYInputWidgetState(hintText, iconData, obscureText);
+  _GSYInputWidgetState createState() => new _GSYInputWidgetState(hintText, iconData, onChanged, textStyle, controller, obscureText);
 }
 
 /// State for [GSYInputWidget] widgets.
 class _GSYInputWidgetState extends State<GSYInputWidget> {
-
   final bool obscureText;
 
   final String hintText;
 
   final IconData iconData;
 
-  final TextEditingController _controller = new TextEditingController();
+  final ValueChanged<String> onChanged;
 
-  _GSYInputWidgetState(this.hintText, this.iconData, this.obscureText) : super();
+  final TextStyle textStyle;
+
+  final TextEditingController controller;
+
+  _GSYInputWidgetState(this.hintText, this.iconData, this.onChanged, this.textStyle, this.controller, this.obscureText) : super();
 
   @override
   Widget build(BuildContext context) {
     return new TextField(
-        controller: _controller,
-        obscureText: obscureText != null ? obscureText: false,
-        decoration: new InputDecoration(
-          hintText: hintText,
-          icon: new Icon(iconData),
-        ),
-      );
+      controller: controller,
+      onChanged: onChanged,
+      obscureText: obscureText,
+      decoration: new InputDecoration(
+        hintText: hintText,
+        icon: new Icon(iconData),
+      ),
+    );
   }
 }
